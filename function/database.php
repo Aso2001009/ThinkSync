ed6525ec642b81dd243108e1918fc512b4cb8c8a
Aso2001009 <83050422+Aso2001009@users.noreply.github.com>
2023-05-10 01:11:55 +0900

    Update database.php

--- function/database.php
+++ function/database.php
@@ -1,23 +1,83 @@
 <?php
-$opt = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXEPTION,
-             PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
-             PDO::ATTR_EMULATE_PREPARES => false
-            );
 
 $pdo = new PDO(
+    'mysql:host=localhost;dbname=;charset=utf8',
     '',
-    '',
-    '',
-    $opt
+    ''
 );
 
 /**
- * データベースを終了する
+ * 未使用のメールアドレスか調べる
+ * @param string $mail
+ * @return bool true:未使用
  */
-function endDB(){
-    global $pdo;
-    $pdo = null;
+function CheckUniqueMail(string $mail) {
+    return SelectUserByMail($mail) === false;
 }
-register_shutdown_function('endDB');
+
+/**
+ * IDでアカウント削除
+ * @param int $id
+ */
+function DeleteUserById(int $user_id){
+    global $pdo;
+    $sql = $pdo->prepare('DELETE FROM user WHERE user_id = :user_id');
+    $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
+    $sql->execute();
+}
+
+/**
+ * アカウントの全件取得
+ * @return array
+ */
+function SelectUserAll(){
+    global $pdo;
+    $sql = $pdo->prepare('SELECT * FROM user');
+    $sql->execute();
+    return $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
+/**
+ * IDでアカウント情報取得
+ * @param int $id
+ * @return array
+ */
+function SelectUserById(int $user_id){
+    global $pdo;
+    $sql = $pdo->prepare('SELECT * FROM user WHERE user_id =:user_id');
+    $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
+    $sql->execute();
+    return $sql->fetch(PDO::FETCH_ASSOC);
+}
+
+/**
+ * メールアドレスでアカウント取得
+ * @param string $mail
+ * @return array
+ */
+function SelectUserByMail(string $mail){
+    global $pdo;
+    $sql = $pdo->prepare('SELECT * FROM user WHERE mail =:mail');
+    $sql->bindValue(':mail',$mail,PDO::PARAM_STR);
+    $sql->execute();
+    return $sql->fetch(PDO::FETCH_ASSOC);
+}
+
+
+/**
+ * [[ここ違うかも]]
+ * メールアドレスとパスワードでアカウント取得
+ * @param string $mail
+ * @param string $pass
+ * @return array
+ */
+function SelectUserByMailAndPass(string $mail,string $pass){
+    global  $pdo;
+    $sql = $pdo->prepare('SELECT * FROM user WHERE mail =:mail && pass =:pass');
+    $sql->bindValue('mail',$mail,PDO::PARAM_STR);
+    $sql->bindValue('pass',$pass,PDO::PARAM_STR);
+    $sql->execute();
+    return $sql->fetch(PDO::FETCH_ASSOC);
+}
+
 ?>
