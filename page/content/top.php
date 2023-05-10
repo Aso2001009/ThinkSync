30e9277c1a168867da6eb3716b7ffbab01ac751f
aso2001036 <83050405+aso2001036@users.noreply.github.com>
2023-05-10 11:31:42 +0900

    Update top.php
    完成品です

---
+++ page/content/top.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ThinkSync</title>
+    <link rel="stylesheet" type="text/css" href="../style/top.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="../script/top.js"></script>
+  </head>
+  <body>
+  <div class="top-main">
+    
+    <p class="top-title">ThinkSync</p>
+    <a href="***" class="top-signin">SignIn</a>
+        <div class="top-header">
+          <a href="***" class="top-host">会議を作成する</a>
+          <a href="***" class="top-guest">会議に参加する</a>
+        </div>
+
+    <div id="top-text" class="top-text">
+      Optimal solution to <br>support <br>
+      the smooth running of <br>meetings
+    </div>
+    
+  </div>
+
+    <!--
+      余裕ができたら実装予定
+      <div class="top-osyare">
+    </div>-->
+
+    <div class="top-footer">
+      <a href="***" class="top-histry">会議履歴を見る</a>
+      <a href="***" class="top-question">お問い合わせ</a>
+      <a href="***" class="top-qa">Q＆A</a>
+      <a href="***" class="top-rule">利用規約</a>
+    </div>
+
+  </body>
+</html>
