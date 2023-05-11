dc62c85f3248606fdd7b6a1e67ab47827d168b3e
Aso2001009 <83050422+Aso2001009@users.noreply.github.com>
2023-05-11 22:22:34 +0900

    Update database.php

--- function/database.php
+++ function/database.php
@@ -1,8 +1,8 @@
 <?php
 
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=;charset=utf8',
-    '',
+    'mysql:host=localhost;dbname=thinksync;charset=utf8',
+    'root',
     ''
 );
 
@@ -13,6 +13,91 @@ $pdo = new PDO(
  */
 function CheckUniqueMail(string $mail) {
     return SelectUserByMail($mail) === false;
+}
+
+/**
+ * パスワードのハッシュ化
+ * @param string $pass
+ * @return false|string|null
+ */
+function HashPass(string $pass){
+    return password_hash($pass,PASSWORD_DEFAULT);
+}
+
+/** ハッシュ値と入力値の比較
+ * @param $pass
+ * @param $hash
+ * @return bool
+ */
+function VerifyPass($pass,$hash){
+    if (password_verify($pass,$hash)) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * アカウントの新規登録
+ * @param string $name
+ * @param string $mail
+ * @param string $pass
+ * @return false|string
+ */
+function InsertUser(string $name,string $mail,string $pass){
+    global $pdo;
+    if(SelectUserByMail($mail) !== false) return false;
+    $sql = $pdo->prepare('INSERT INTO user VALUES(?,?,?,?)');
+    $sql->bindValue($name,PDO::PARAM_STR);
+    $sql->bindValue($mail,PDO::PARAM_STR);
+    $sql->bindValue($pass,PDO::PARAM_STR);
+    $sql->execute(array(null,$name,$mail,$pass));
+    return $pdo->lastInsertId();
+}
+
+/**
+ * アカウント名の変更
+ * @param string $name
+ * @param int $user_id
+ * @return bool
+ */
+function UpdateUserName(string $name,int $user_id){
+    global $pdo;
+    $sql = $pdo->prepare('UPDATE user SET name = ? WHERE user_id = ?');
+    $sql->bindValue($name,PDO::PARAM_STR);
+    $sql->bindValue($user_id,PDO::PARAM_INT);
+    $sql->execute(array($name,$user_id,));
+    return true;
+}
+
+/**
+ * アカウントのメールアドレスの変更
+ * @param string $mail
+ * @param int $user_id
+ * @return bool
+ */
+function UpdateUserMail(string $mail,int $user_id){
+    global $pdo;
+    if (CheckUniqueMail($mail) === false) return false;
+    $sql = $pdo->prepare('UPDATE user SET mail = ? WHERE user_id = ?');
+    $sql->bindValue($mail,PDO::PARAM_STR);
+    $sql->bindValue($user_id,PDO::PARAM_INT);
+    $sql->execute(array($mail,$user_id,));
+    return true;
+}
+
+/**
+ * アカウントのパスワードの変更
+ * @param string $pass
+ * @param int $user_id
+ * @return bool
+ */
+function UpdateUserPass(string $pass,int $user_id){
+    global $pdo;
+    $sql = $pdo->prepare('UPDATE user SET pass = ? WHERE user_id = ?');
+    $sql->bindValue($pass,PDO::PARAM_STR);
+    $sql->bindValue($user_id,PDO::PARAM_INT);
+    $sql->execute(array($pass,$user_id,));
+    return true;
 }
 
 /**
@@ -65,7 +150,6 @@ function SelectUserByMail(string $mail){
 
 
 /**
- * [[ここ違うかも]]
  * メールアドレスとパスワードでアカウント取得
  * @param string $mail
  * @param string $pass
@@ -79,5 +163,4 @@ function SelectUserByMailAndPass(string $mail,string $pass){
     $sql->execute();
     return $sql->fetch(PDO::FETCH_ASSOC);
 }
-
 ?>
