6db1895f01cf4f900b051d451557d83e311a7d6e
春田駿佑 <131325311+2201906@users.noreply.github.com>
2023-07-06 11:32:31 +0900

    Add files via upload

---
+++ page/content/home_temple.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/home_temple.css">
+</head>
+<body>
+    <!-- 画面上部-->
+    <div class="top">
+        <!-- 画面左上アプリロゴ画像-->
+        <div class="logo">
+            <img src="img/logo.png" alt="アプリロゴ">
+        </div>
+        <!-- 画面右上「ログイン/新規登録」-->
+        <div class="login">
+            <a href="login.php">ログイン/新規登録</a>
+        </div>
+    </div>
+
+    <div class="banner">
+        <div class="app-links">
+            <a href="create_meeting.php">会議を作成</a>
+            <a href="join_meeting.php">会議に参加</a>
+            <a href="history.php">履歴</a>
+        </div>
+    </div>
+    <!--　画面下部-->
+    <!--  左のbannerに干渉しないようにする-->
+    <!-- 項目はお問い合わせ、利用規約、Q&A-->
+    <div class="bottom">
+        <div class="bottom-links">
+            <a href="contact.php">お問い合わせ</a>
+            <a href="terms_of_service.php">利用規約</a>
+            <a href="Q&A.php">Q&A</a>
+        </div>
+</body>
+</html>
