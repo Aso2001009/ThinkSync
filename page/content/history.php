1365c154ec7dd66d1f95b54a7adb02c709c3360c
Aso2201902 <83050649+Aso2001031@users.noreply.github.com>
2023-07-07 12:19:30 +0900

    Add files via upload

--- page/content/history.php
+++ page/content/history.php
@@ -12,60 +12,61 @@
     <script type="text/javascript" src="../script/top.js"></script>
   </head>
   <body>
-  <?php
-  function connectDB(){
-    $opt = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+    <?php
+      function connectDB(){
+      $opt = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                  PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
                  PDO::ATTR_EMULATE_PREPARES => false);
   
-    $db = new PDO(
+      $db = new PDO(
           'mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1530414-thinksync;charset=utf8',
           'LAA1530414',
           'SD3TS',
         $opt
-      );
+        );
 
-      return $db;
-  }
-  ?>
+        return $db;
+      }
+    ?>
 
-  <div class="top-main">
+<a class="page_title">会議履歴</a>
 
-    <a class="page_title">会議履歴</a>
+    <div class="scrollbar_content">
 
-    <?php
-    try{
-      $db = connectDB();
-      //logsテーブルとroomsテーブルを結合して、ログインユーザーの会議履歴を取得
-      $sql = "SELECT * FROM logs JOIN rooms ON logs.room_id = rooms.room_id WHERE logs.user_id = ? ";
-      $ps = $db->prepare($sql);
-	    $ps->bindValue(1, $user_id, PDO::PARAM_INT);
-	    $ps->execute();
+      <?php
+      try{
+        $db = connectDB();
+        //logsテーブルとroomsテーブルを結合して、ログインユーザーの会議履歴を取得
+        $sql = "SELECT * FROM logs JOIN rooms ON logs.room_id = rooms.room_id WHERE logs.user_id = ? ";
+        $ps = $db->prepare($sql);
+	      $ps->bindValue(1, $user_id, PDO::PARAM_INT);
+	      $ps->execute();
 
-      // 会議の参加履歴がある場合1件ずつ取り出して表示
-      $counter = 0;
-      echo '<div class="history_content_row">'; // 最初の行を追加
-      while ($row = $ps->fetch()) {
-        if ($counter > 0 && $counter % 3 === 0) {
-          echo '</div>'; // 3列ごとに行を閉じる
-          echo '<div class="row">'; // 新しい行を開始
+        // 会議の参加履歴がある場合1件ずつ取り出して表示
+        $counter = 0;
+        echo '<div class="history_content_row">'; // 最初の行を追加
+        while ($row = $ps->fetch()) {
+          if ($counter > 0 && $counter % 3 === 0) {
+            echo '</div>'; // 3列ごとに行を閉じる
+            echo '<div class="history_content_row">'; // 新しい行を開始
+          }
+
+          echo '<div class="history_content">';
+          echo '<div class="history_content_name"><a>' . $row['title'] . '</a></div>';
+          echo '<div class="history_content_day"><a>' . $row['end'] . '</a></div>';
+          echo '</div>';
+
+          $counter++;
         }
 
-        echo '<div class="history_content">';
-        echo '<div class="history_content_name"><a>' . $row['title'] . '</a></div>';
-        echo '<div class="history_content_day"><a>' . $row['end'] . '</a></div>';
-        echo '</div>';
+        echo '</div>'; // 最後の行を閉じる
 
-        $counter++;
+      }catch(PDOException $e){
+        die("エラーメッセージ：{$e->getMessage()}");
       }
+      ?>
 
-      echo '</div>'; // 最後の行を閉じる
-
-    }catch(PDOException $e){
-      die("エラーメッセージ：{$e->getMessage()}");
-    }
-    
-    ?>
-  </div>
+    </div>
   </body>
 </html>
