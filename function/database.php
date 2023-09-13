8c34535d6bd0c961bb15db78e6d08b97cdd70103
Aso2001009 <83050422+Aso2001009@users.noreply.github.com>
2023-09-13 10:08:42 +0900

    Add files via upload

--- function/database.php
+++ function/database.php
@@ -1,166 +1,99 @@
 <?php
 
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=thinksync;charset=utf8',
-    'root',
-    ''
+    'mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1530414-thinksync;charset=utf8',
+    'LAA1530414',
+    'SD3TS'
 );
-
 /**
- * 未使用のメールアドレスか調べる
- * @param string $mail
- * @return bool true:未使用
+ * room_idを生成
+ * @return string
  */
-function CheckUniqueMail(string $mail) {
-    return SelectUserByMail($mail) === false;
-}
-
-/**
- * パスワードのハッシュ化
- * @param string $pass
- * @return false|string|null
- */
-function HashPass(string $pass){
-    return password_hash($pass,PASSWORD_DEFAULT);
-}
-
-/** ハッシュ値と入力値の比較
- * @param $pass
- * @param $hash
- * @return bool
- */
-function VerifyPass($pass,$hash){
-    if (password_verify($pass,$hash)) {
-        return true;
+function CreateRoomId(){
+    $room_id = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 5);
+    if(CheckRoomId($room_id)){
+        return $room_id;
+    }else{
+        CreateRoomId();
     }
-    return false;
 }
 
 /**
- * アカウントの新規登録
- * @param string $name
- * @param string $mail
- * @param string $pass
- * @return false|string
+ * 重複するroom_idがないか確認
+ * @param string $room_id
+ * @return bool
  */
-function InsertUser(string $name,string $mail,string $pass){
+function CheckRoomId($room_id){
     global $pdo;
-    if(SelectUserByMail($mail) !== false) return false;
-    $sql = $pdo->prepare('INSERT INTO user VALUES(?,?,?,?)');
-    $sql->bindValue($name,PDO::PARAM_STR);
-    $sql->bindValue($mail,PDO::PARAM_STR);
-    $sql->bindValue($pass,PDO::PARAM_STR);
-    $sql->execute(array(null,$name,$mail,$pass));
-    return $pdo->lastInsertId();
+    $sql = $pdo->prepare('SELECT * FROM rooms WHERE room_id = :room_id');
+    $sql->bindValue(':room_id',$room_id,PDO::PARAM_STR);
+    $sql->execute();
+    $result = $sql->fetch(PDO::FETCH_ASSOC);
+    if($result == false){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 /**
- * アカウント名の変更
- * @param string $name
+ * 会議作成
+ * @param string $room_id
+ * @param int $user_id
+ * @param string $title
+ * @return bool
+ */
+function CreateRoom($room_id,$user_id,$title){
+    global $pdo;
+    $sql = $pdo->prepare('INSERT INTO rooms(room_id,user_id,title,temp,start,end) VALUES(:room_id,:user_id,:title,NULL,:start,NULL)');
+    $sql->bindValue(':room_id',$room_id,PDO::PARAM_STR);
+    $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
+    $sql->bindValue(':title',$title,PDO::PARAM_STR);
+    $sql->bindValue(':start',date('Y-m-d H:i:s'),PDO::PARAM_STR);
+    $sql->execute();
+}
+
+/**
+ * 重複するログがないか確認
+ * @param string $room_id
  * @param int $user_id
  * @return bool
  */
-function UpdateUserName(string $name,int $user_id){
+function CheckLog($room_id,$user_id){
     global $pdo;
-    $sql = $pdo->prepare('UPDATE user SET name = ? WHERE user_id = ?');
-    $sql->bindValue($name,PDO::PARAM_STR);
-    $sql->bindValue($user_id,PDO::PARAM_INT);
-    $sql->execute(array($name,$user_id,));
-    return true;
-}
-
-/**
- * アカウントのメールアドレスの変更
- * @param string $mail
- * @param int $user_id
- * @return bool
- */
-function UpdateUserMail(string $mail,int $user_id){
-    global $pdo;
-    if (CheckUniqueMail($mail) === false) return false;
-    $sql = $pdo->prepare('UPDATE user SET mail = ? WHERE user_id = ?');
-    $sql->bindValue($mail,PDO::PARAM_STR);
-    $sql->bindValue($user_id,PDO::PARAM_INT);
-    $sql->execute(array($mail,$user_id,));
-    return true;
-}
-
-/**
- * アカウントのパスワードの変更
- * @param string $pass
- * @param int $user_id
- * @return bool
- */
-function UpdateUserPass(string $pass,int $user_id){
-    global $pdo;
-    $sql = $pdo->prepare('UPDATE user SET pass = ? WHERE user_id = ?');
-    $sql->bindValue($pass,PDO::PARAM_STR);
-    $sql->bindValue($user_id,PDO::PARAM_INT);
-    $sql->execute(array($pass,$user_id,));
-    return true;
-}
-
-/**
- * IDでアカウント削除
- * @param int $id
- */
-function DeleteUserById(int $user_id){
-    global $pdo;
-    $sql = $pdo->prepare('DELETE FROM user WHERE user_id = :user_id');
+    $sql = $pdo->prepare('SELECT * FROM logs WHERE room_id = :room_id && user_id = :user_id');
+    $sql->bindValue(':room_id',$room_id,PDO::PARAM_STR);
     $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
     $sql->execute();
+    $result = $sql->fetch(PDO::FETCH_ASSOC);
+    if($result == false){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 /**
- * アカウントの全件取得
- * @return array
+ * ログ作成
+ * @param string $room_id
+ * @param int $user_id
+ * @return bool
  */
-function SelectUserAll(){
+function InsertLog($room_id,$user_id){
+    if(CheckLog($room_id,$user_id)){
+        global $pdo;
+        $sql = $pdo->prepare('INSERT INTO logs(room_id,user_id) VALUES(:room_id,:user_id)');
+        $sql->bindValue(':room_id',$room_id,PDO::PARAM_STR);
+        $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
+        $sql->execute();
+    }
+}
+
+function UpdateRoom($room_id){
     global $pdo;
-    $sql = $pdo->prepare('SELECT * FROM user');
+    $sql = $pdo->prepare('UPDATE rooms SET end = :end WHERE room_id = :room_id');
+    $sql->bindValue(':room_id',$room_id,PDO::PARAM_STR);
+    $sql->bindValue(':end',date('Y-m-d H:i:s'),PDO::PARAM_STR);
     $sql->execute();
-    return $sql->fetchAll(PDO::FETCH_ASSOC);
-}
-
-/**
- * IDでアカウント情報取得
- * @param int $id
- * @return array
- */
-function SelectUserById(int $user_id){
-    global $pdo;
-    $sql = $pdo->prepare('SELECT * FROM user WHERE user_id =:user_id');
-    $sql->bindValue(':user_id',$user_id,PDO::PARAM_INT);
-    $sql->execute();
-    return $sql->fetch(PDO::FETCH_ASSOC);
-}
-
-/**
- * メールアドレスでアカウント取得
- * @param string $mail
- * @return array
- */
-function SelectUserByMail(string $mail){
-    global $pdo;
-    $sql = $pdo->prepare('SELECT * FROM user WHERE mail =:mail');
-    $sql->bindValue(':mail',$mail,PDO::PARAM_STR);
-    $sql->execute();
-    return $sql->fetch(PDO::FETCH_ASSOC);
-}
-
-
-/**
- * メールアドレスとパスワードでアカウント取得
- * @param string $mail
- * @param string $pass
- * @return array
- */
-function SelectUserByMailAndPass(string $mail,string $pass){
-    global  $pdo;
-    $sql = $pdo->prepare('SELECT * FROM user WHERE mail =:mail && pass =:pass');
-    $sql->bindValue('mail',$mail,PDO::PARAM_STR);
-    $sql->bindValue('pass',$pass,PDO::PARAM_STR);
-    $sql->execute();
-    return $sql->fetch(PDO::FETCH_ASSOC);
 }
 ?>
